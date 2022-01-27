f27905cd824ec6db2a9ecc688e6fd64738d5d394
Mister-Hope <mister-hope@outlook.com>
2022-01-27 13:28:25 +0000

    Deploying to gh-pages from @ vuepress-theme-hope/vuepress-theme-hope@1be5b7c847fd9527edd50a5cd0eda49000519ca9 🚀

---
+++ md-enhance/assets/container.html.a27ea017.js
@@ -0,0 +1,59 @@
+import{j as n}from"./app.2200a787.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const a={},e=n(`<p>\u63D2\u4EF6\u53EF\u4EE5\u4E3A\u4F60\u6DFB\u52A0\u63D0\u793A\u3001\u6CE8\u91CA\u3001\u4FE1\u606F\u3001\u6CE8\u610F\u3001\u8B66\u544A\u548C\u8BE6\u60C5\u81EA\u5B9A\u4E49\u5BB9\u5668\u7684\u652F\u6301\u3002</p><h2 id="\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#\u914D\u7F6E" aria-hidden="true">#</a> \u914D\u7F6E</h2><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code>module<span class="token punctuation">.</span>exports <span class="token operator">=</span> <span class="token punctuation">{</span>
+  <span class="token literal-property property">plugins</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">[</span>
+      <span class="token string">&quot;md-enhance&quot;</span><span class="token punctuation">,</span>
+      <span class="token punctuation">{</span>
+        <span class="token comment">// \u542F\u7528\u81EA\u5B9A\u4E49\u5BB9\u5668</span>
+        <span class="token literal-property property">container</span><span class="token operator">:</span> <span class="token boolean">true</span><span class="token punctuation">,</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre><div class="highlight-lines"><br><br><br><br><br><br><div class="highlight-line">\xA0</div><br><br><br><br></div><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br></div></div><h2 id="\u6F14\u793A" tabindex="-1"><a class="header-anchor" href="#\u6F14\u793A" aria-hidden="true">#</a> \u6F14\u793A</h2><div class="custom-container info"><p class="custom-container-title">\u76F8\u5173\u4FE1\u606F</p><p>\u4FE1\u606F\u5BB9\u5668\u3002</p></div><div class="custom-container note"><p class="custom-container-title">\u6CE8</p><p>\u6CE8\u91CA\u5BB9\u5668\u3002</p></div><div class="custom-container tip"><p class="custom-container-title">\u63D0\u793A</p><p>\u63D0\u793A\u5BB9\u5668</p></div><div class="custom-container warning"><p class="custom-container-title">\u6CE8\u610F</p><p>\u8B66\u544A\u5BB9\u5668</p></div><div class="custom-container danger"><p class="custom-container-title">\u76F8\u5173\u4FE1\u606F</p><p>\u5371\u9669\u5BB9\u5668</p></div><details class="custom-block details"><summary>\u8BE6\u60C5</summary><p>\u8BE6\u60C5\u5BB9\u5668</p></details><div class="custom-container info"><p class="custom-container-title">\u81EA\u5B9A\u4E49\u6807\u9898</p><p>\u4FE1\u606F\u5BB9\u5668</p></div><div class="custom-container note"><p class="custom-container-title">\u81EA\u5B9A\u4E49\u6807\u9898</p><p>\u6CE8\u91CA\u5BB9\u5668</p></div><div class="custom-container tip"><p class="custom-container-title">\u81EA\u5B9A\u4E49\u6807\u9898</p><p>\u63D0\u793A\u5BB9\u5668</p></div><div class="custom-container warning"><p class="custom-container-title">\u81EA\u5B9A\u4E49\u6807\u9898</p><p>\u8B66\u544A\u5BB9\u5668</p></div><div class="custom-container danger"><p class="custom-container-title">\u81EA\u5B9A\u4E49\u6807\u9898</p><p>\u5371\u9669\u5BB9\u5668</p></div><details class="custom-block details"><summary>\u81EA\u5B9A\u4E49\u6807\u9898</summary><p>\u8BE6\u60C5\u5BB9\u5668</p></details><div class="language-markdown ext-md line-numbers-mode"><pre class="language-markdown"><code>::: info
+\u4FE1\u606F\u5BB9\u5668\u3002
+:::
+
+::: note
+\u6CE8\u91CA\u5BB9\u5668\u3002
+:::
+
+::: tip
+\u63D0\u793A\u5BB9\u5668
+:::
+
+::: warning
+\u8B66\u544A\u5BB9\u5668
+:::
+
+::: danger
+\u5371\u9669\u5BB9\u5668
+:::
+
+::: details
+\u8BE6\u60C5\u5BB9\u5668
+:::
+
+::: info \u81EA\u5B9A\u4E49\u6807\u9898
+\u4FE1\u606F\u5BB9\u5668
+:::
+
+::: note \u81EA\u5B9A\u4E49\u6807\u9898
+\u6CE8\u91CA\u5BB9\u5668
+:::
+
+::: tip \u81EA\u5B9A\u4E49\u6807\u9898
+\u63D0\u793A\u5BB9\u5668
+:::
+
+::: warning \u81EA\u5B9A\u4E49\u6807\u9898
+\u8B66\u544A\u5BB9\u5668
+:::
+
+::: danger \u81EA\u5B9A\u4E49\u6807\u9898
+\u5371\u9669\u5BB9\u5668
+:::
+
+::: details \u81EA\u5B9A\u4E49\u6807\u9898
+\u8BE6\u60C5\u5BB9\u5668
+:::
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br><span class="line-number">25</span><br><span class="line-number">26</span><br><span class="line-number">27</span><br><span class="line-number">28</span><br><span class="line-number">29</span><br><span class="line-number">30</span><br><span class="line-number">31</span><br><span class="line-number">32</span><br><span class="line-number">33</span><br><span class="line-number">34</span><br><span class="line-number">35</span><br><span class="line-number">36</span><br><span class="line-number">37</span><br><span class="line-number">38</span><br><span class="line-number">39</span><br><span class="line-number">40</span><br><span class="line-number">41</span><br><span class="line-number">42</span><br><span class="line-number">43</span><br><span class="line-number">44</span><br><span class="line-number">45</span><br><span class="line-number">46</span><br><span class="line-number">47</span><br></div></div>`,17);function p(r,l){return e}var t=s(a,[["render",p]]);export{t as default};
---
+++ reading-time/assets/index.html.8476fbac.js
@@ -0,0 +1,45 @@
+import{r as l,o as c,a as i,b as a,w as e,F as r,d as o,e as n,f as s}from"./app.19d5c686.js";import{_ as u}from"./plugin-vue_export-helper.21dcd24c.js";const d={},m=o(`<p>This plugin will inject expect reading time and word count statistics into the page object.</p><p>Will automatically inject <code>readingTime</code> into the page object:</p><div class="language-typescript ext-ts line-numbers-mode"><pre class="language-typescript"><code><span class="token keyword">interface</span> <span class="token class-name">ReadingTime</span> <span class="token punctuation">{</span>
+  <span class="token doc-comment comment">/** Expect reading minute */</span>
+  minutes<span class="token operator">:</span> <span class="token builtin">number</span><span class="token punctuation">;</span>
+  <span class="token doc-comment comment">/** Words count */</span>
+  words<span class="token operator">:</span> <span class="token builtin">number</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br></div></div><h2 id="how-to-use" tabindex="-1"><a class="header-anchor" href="#how-to-use" aria-hidden="true">#</a> How to use</h2><h3 id="install" tabindex="-1"><a class="header-anchor" href="#install" aria-hidden="true">#</a> Install</h3>`,5),b=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"yarn"),s(),n("span",{class:"token function"},"add"),s(` -D vuepress-plugin-reading-time2
+`)])]),n("div",{class:"line-numbers"},[n("span",{class:"line-number"},"1"),n("br")])],-1),k=n("div",{class:"language-bash ext-sh line-numbers-mode"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"npm"),s(` i -D vuepress-plugin-reading-time2
+`)])]),n("div",{class:"line-numbers"},[n("span",{class:"line-number"},"1"),n("br")])],-1),g=n("h3",{id:"usage",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#usage","aria-hidden":"true"},"#"),s(" Usage")],-1),h=n("div",{class:"language-typescript ext-ts line-numbers-mode"},[n("pre",{class:"language-typescript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.ts"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" readingTime "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress-plugin-reading-time2"'),n("span",{class:"token punctuation"},";"),s(`
+
+`),n("span",{class:"token keyword"},"export"),s(),n("span",{class:"token keyword"},"default"),s(),n("span",{class:"token punctuation"},"{"),s(`
+  plugins`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"["),s(`
+    `),n("span",{class:"token function"},"readingTime"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+      `),n("span",{class:"token comment"},"// your options"),s(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),s(`
+  `),n("span",{class:"token punctuation"},"]"),n("span",{class:"token punctuation"},","),s(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),s(`
+`)])]),n("div",{class:"line-numbers"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br"),n("span",{class:"line-number"},"6"),n("br"),n("span",{class:"line-number"},"7"),n("br"),n("span",{class:"line-number"},"8"),n("br"),n("span",{class:"line-number"},"9"),n("br"),n("span",{class:"line-number"},"10"),n("br")])],-1),f=n("div",{class:"language-javascript ext-js line-numbers-mode"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.js"),s(`
+`),n("span",{class:"token keyword"},"const"),s(),n("span",{class:"token punctuation"},"{"),s(" readingTime "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token operator"},"="),s(),n("span",{class:"token function"},"require"),n("span",{class:"token punctuation"},"("),n("span",{class:"token string"},'"vuepress-plugin-reading-time2"'),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},";"),s(`
+
+module`),n("span",{class:"token punctuation"},"."),s("exports "),n("span",{class:"token operator"},"="),s(),n("span",{class:"token punctuation"},"{"),s(`
+  `),n("span",{class:"token literal-property property"},"plugins"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"["),s(`
+    `),n("span",{class:"token function"},"readingTime"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+      `),n("span",{class:"token comment"},"// your options"),s(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),s(`
+  `),n("span",{class:"token punctuation"},"]"),n("span",{class:"token punctuation"},","),s(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),s(`
+`)])]),n("div",{class:"line-numbers"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br"),n("span",{class:"line-number"},"6"),n("br"),n("span",{class:"line-number"},"7"),n("br"),n("span",{class:"line-number"},"8"),n("br"),n("span",{class:"line-number"},"9"),n("br"),n("span",{class:"line-number"},"10"),n("br")])],-1),_=o(`<h2 id="plugin-options" tabindex="-1"><a class="header-anchor" href="#plugin-options" aria-hidden="true">#</a> Plugin options</h2><h3 id="wordperminute" tabindex="-1"><a class="header-anchor" href="#wordperminute" aria-hidden="true">#</a> wordPerminute</h3><ul><li>Type: <code>number</code></li><li>Default: <code>300</code></li></ul><p>Reading speed (words per minute)</p><h3 id="locales" tabindex="-1"><a class="header-anchor" href="#locales" aria-hidden="true">#</a> locales</h3><div class="language-typescript ext-ts line-numbers-mode"><pre class="language-typescript"><code><span class="token keyword">interface</span> <span class="token class-name">ReadingTimeLocaleData</span> <span class="token punctuation">{</span>
+  <span class="token doc-comment comment">/**
+   * Word template, \`$word\` will be automatically replaced by actual words
+   */</span>
+  word<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">;</span>
+
+  <span class="token doc-comment comment">/**
+   * Text for less than one minute
+   */</span>
+  less1Minute<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">;</span>
+
+  <span class="token doc-comment comment">/**
+   * Time template
+   */</span>
+  time<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br></div></div><p>Locales config.</p>`,7);function v(x,w){const t=l("CodeGroupItem"),p=l("CodeGroup");return c(),i(r,null,[m,a(p,null,{default:e(()=>[a(t,{title:"yarn"},{default:e(()=>[b]),_:1}),a(t,{title:"npm"},{default:e(()=>[k]),_:1})]),_:1}),g,a(p,null,{default:e(()=>[a(t,{title:"ts"},{default:e(()=>[h]),_:1}),a(t,{title:"js"},{default:e(()=>[f]),_:1})]),_:1}),_],64)}var j=u(d,[["render",v]]);export{j as default};
