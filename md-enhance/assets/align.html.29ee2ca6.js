9ebca4cdbf5f0d10b177b97994d0b38d22390723
Mister-Hope <mister-hope@outlook.com>
2022-02-06 12:47:30 +0000

    Deploying to gh-pages from @ vuepress-theme-hope/vuepress-theme-hope@fdec316ea1b09a29928785cc39cdfc0b14dfdfaf 🚀

---
+++ md-enhance/assets/align.html.29ee2ca6.js
@@ -0,0 +1,10 @@
+const e={key:"v-22e17240",path:"/guide/align.html",title:"Custom alignment",lang:"en-US",frontmatter:{title:"Custom alignment",icon:"align",head:[["meta",{property:"og:url",content:"https://vuepress-theme-hope.github.io/v2/md-enhance/guide/align.html"}],["meta",{property:"og:title",content:"Custom alignment"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{name:"twitter:card",content:"summary_large_image"}],["script",{type:"application/ld+json"},'{"@context":"https://schema.org","@type":"NewsArticle","headline":"Custom alignment","image":[""],"datePublished":"","dateModified":"","author":[]}']]},excerpt:`<p>By injecting configuration into vuepress-plugin-container, you can use</p>
+<div class="language-markdown ext-md line-numbers-mode"><pre v-pre class="language-markdown"><code>::: center
+Paragraph to center
+:::
+
+::: right
+Right paragraph
+:::
+</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br></div></div><p>To customize your paragraph alignment.</p>
+`,headers:[{level:2,title:"Config",slug:"config",children:[]},{level:2,title:"Demo",slug:"demo",children:[]}],git:{createdTime:1644151125e3,updatedTime:1644151125e3,contributors:[{name:"Mr.Hope",email:"zhangbowang1998@gmail.com",commits:1}]},readingTime:{minutes:.4,words:119},filePathRelative:"guide/align.md"};export{e as data};
